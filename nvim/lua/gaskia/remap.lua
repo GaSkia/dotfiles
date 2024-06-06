a74d390f671ba58d2cc86453af36f5dbcc779e18
GaSkia <gabry.schiavo00@gmail.com>
2024-06-06 22:55:44 +0200

    added harpoon.lua treesitter.lua set.lua and resources folder, modified hyprpaper.conf (see resources/2.png) telescope.lua remap.lua waybar/launch.sh (added lines for hyprpaper)

--- nvim/lua/gaskia/remap.lua
+++ nvim/lua/gaskia/remap.lua
@@ -1,2 +1,9 @@
 vim.g.mapleader = ';'
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>rn', function()
+	if (vim.o.relativenumber) then
+		vim.o.relativenumber = false
+	else
+		vim.o.relativenumber = true
+	end
+end)
--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 killall waybar
-
+killall hyprpaper
 if [[ $USER = "gaskia" ]]
 then
     waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
 else
     waybar &
 fi
+
+hyprpaper &
