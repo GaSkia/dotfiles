429d84bed5d0d106eb987ccb4cd3bb0bfbdeff3f
GaSkia <gabry.schiavo00@gmail.com>
2024-10-20 14:26:32 +0200

    modified:   lua/gaskia/remap.lua         changed neotree to toggle mode

--- nvim/lua/gaskia/remap.lua
+++ nvim/lua/gaskia/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ';'
 vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
-vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left<CR>')
-vim.keymap.set('n', '<leader>T', ':Neotree filesystem reveal float<CR>')
+vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left toggle<CR>')
+vim.keymap.set('n', '<leader>T', ':Neotree filesystem reveal float toggle<CR>')
 vim.keymap.set('n', '<C-e>', function() vim.diagnostic.float() end)
 vim.keymap.set('n', '<leader>ne', function() vim.diagnostic.goto_next() end)
 vim.keymap.set('n', '<leader>pe', function() vim.diagnostic.goto_prev() end)
