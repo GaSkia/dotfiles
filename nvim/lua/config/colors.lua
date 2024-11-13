159db80ddb3ddeb8509bf42489626db0bc85c94e
GaSkia <gabry.schiavo00@gmail.com>
2024-11-13 07:31:55 +0100

    migrated to lazy

---
+++ nvim/lua/config/colors.lua
@@ -0,0 +1,8 @@
+function ColorMeMaster(color)
+	color = color or 'tokyonight'
+
+	vim.api.nvim_set_hl(0, 'normal', {bg = 'none'})
+	vim.api.nvim_set_hl(0, 'normalFloat', {bg = 'none'})
+end
+
+ColorMeMaster()
