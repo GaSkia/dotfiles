87a9d10ec655b58e928f74795d577a4691e3f157
GaSkia <gabry.schiavo00@gmail.com>
2024-12-03 07:22:39 +0000

    refactored neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -5,8 +5,24 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-      },
-      config = function ()
-	      vim.keymap.set('n', '<leader>t', ':Neotree filesystem reveal left toggle=true<CR>')
-      end,
+	},
+
+	opts = {},
+	keys = {
+		{
+			"<leader>t",
+			function()
+				require("neo-tree.command").execute({toggle = true, dir = vim.uv.cwd() })
+			end,
+			desc = "Explorer NeoTree (cwd)",
+		},
+
+	},
+	config = function(_, opts)
+		local events = require("neo-tree.events")
+		opts.event_handlers = opts.event_handlers or {}
+		vim.list_extend(opts.event_handlers, {
+			event = events.FILE_OPENED
+		})
+	end,
 }
