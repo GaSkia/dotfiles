17490ebc9391f3b99c210b7f4cdbd94e708e9a06
GaSkia <gabry.schiavo00@gmail.com>
2024-12-04 19:17:18 +0000

    rebased cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,4 +1,27 @@
 --TODO: implement nvim-cmp
+--TODO: provide snipped.expand function
+--NOTE: disable default option
 return {
 	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+
+	opts = function()
+		local cmp = require("cmp")
+		
+		return {
+			mapping = cmp.mapping.preset.insert({
+				["C-n"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["C-p"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-Space>"] = cmp.mapping.complete(),
+			}),
+			["<C-CR>"] = function(fallback)
+				cmp.abort()
+				fallback()
+			end,
+		}
+	end,
 }
