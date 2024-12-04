64599e644684a03d9660af69fe581145314dff48
GaSkia <gabry.schiavo00@gmail.com>
2024-12-04 22:19:08 +0100

    modified:   lua/config/setup.lua 	modified:   lua/plugins/mason.lua 	modified:   lua/plugins/nvim-cmp.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -15,10 +15,5 @@ return {
 	    require("mason").setup()
 	    require("mason-lspconfig").setup({
 	    }) 
-
-	    -- After setting up mason-lspconfig you may set up servers via lspconfig
-	    -- require("lspconfig").lua_ls.setup {}
-	    -- require("lspconfig").rust_analyzer.setup {}
-	    -- ...
     end,
 }
