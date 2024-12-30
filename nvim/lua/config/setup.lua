8de986cade3ccf1b761ae070180208daf06f7d43
GaSkia <gabry.schiavo00@gmail.com>
2024-12-30 12:14:35 +0100

    added cmp and completions

--- nvim/lua/config/setup.lua
+++ nvim/lua/config/setup.lua
@@ -6,3 +6,13 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
+vim.diagnostic.config({
+   virtual_text = false,
+   always = true,
+   float = {
+       border = "rounded",
+       source = "always",
+   },
+})
+vim.keymap.set('n', '<leader>ne', function() vim.diagnostic.goto_next() end)
+vim.keymap.set('n', '<leader>pe', function() vim.diagnostic.goto_prev() end)
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -9,19 +9,44 @@ return {
 		"hrsh7th/cmp-path",
 	},
 
-	opts = function()
-		local cmp = require("cmp")
-		
-		return {
-			mapping = cmp.mapping.preset.insert({
-				["C-n"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-				["C-p"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-				["<C-Space>"] = cmp.mapping.complete(),
-			}),
-			["<C-CR>"] = function(fallback)
-				cmp.abort()
-				fallback()
-			end,
-		}
-	end,
+--	opts = function()
+--		local cmp = require("cmp")
+--
+--        cmp.config.disable = true
+--		return {
+--			mapping = cmp.mapping.preset.insert({
+--				["C-n"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+--				["C-p"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+--				["<C-Space>"] = cmp.mapping.complete(),
+--			}),
+--			["<C-CR>"] = function(fallback)
+--				cmp.abort()
+--				fallback()
+--			end,
+--		}
+--	end,
+    config = function()
+        local cmp = require'cmp'
+
+        cmp.setup({
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-n>'] = cmp.mapping.select_next_item({behavior = cmp.SelectBehavior.Select }),
+                ['<C-p>'] = cmp.mapping.select_prev_item({behavior = cmp.SelectBehavior.Select }),
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            }),
+
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'buffer' },
+            }),
+        })
+
+    end,
 }
