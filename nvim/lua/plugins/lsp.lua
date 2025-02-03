769c13e33f6dd8641a5e408f08d6abaeff9e05a5
GaSkia <gabry.schiavo00@gmail.com>
2025-02-03 16:10:44 +0100

    removed unused languages

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,7 +10,11 @@ return {
             ensure_installed = {
                 "pylsp",
                 "html",
-                ""
+                "clangd",
+                "lua_ls",
+                "cssls",
+                "jsonls",
+                "ts_ls",
             }
         },
         config = function()
@@ -31,68 +35,5 @@ return {
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
         },
-        opts = {
-            servers = {
-                omnisharp = {
-                    handlers = {
-                        ["textDocument/definition"] = function(...)
-                            return require("omnisharp_extended").handler(...)
-                        end,
-                    },
-                },
-                enable_roslyn_analyzers = true,
-                organize_imports_on_format = true,
-                enable_import_completion = true,
-            },
-        },
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require'cmp_nvim_lsp'.default_capabilities()
-            local on_attach = function()
-                vim.keymap.set('n', '<leader>rr', vim.lsp.buf.rename, {})
-            end
-
-            require("mason-lspconfig").setup_handlers({
-                function(server_name)
-                    lspconfig[server_name].setup({})
-                end,
-            })
-
-            lspconfig.lua_ls.setup({
-                settings = {
-                    diagnostic = {
-                        globals = { "vim" },
-                        capabilities = capabilities,
-                    }
-                }
-            })
-
-            lspconfig.omnisharp.setup({
-                cmd = { "/home/gaskia/.local/share/nvim/mason/bin/omnisharp","dotnet", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
-                root_dir = lspconfig.util.root_pattern("*.csproj", "*.sln", ".git"),
-                capabilities = require("cmp_nvim_lsp").default_capabilities(),
-                settings = {
-                    globals = { "vim" },
-                    capabilities = capabilities,
-                }
-            })
-            lspconfig.html.setup({
-                settings = {
-                    diagnostic = {
-                        globals = { "vim" },
-                        capabilities = capabilities,
-                    }
-                }
-            })
-
-            lspconfig.cssls.setup({
-                settings = {
-                    diagnostic = {
-                        globals = { "vim" },
-                        capabilities = capabilities,
-                    },
-                },
-            })
-        end,
-    }
+    },
 }
