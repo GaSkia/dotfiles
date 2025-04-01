01900ff679b90cc1feb53697d7c2970dda9f75ac
GaSkia <gabry.schiavo00@gmail.com>
2025-04-01 16:27:47 +0200

    added brave:   .gitignore

--- nvim/lua/plugins/flutter.lua
+++ nvim/lua/plugins/flutter.lua
@@ -5,5 +5,35 @@ return {
         'nvim-lua/plenary.nvim',
         'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
-    config = true,
+
+    config = function()
+        require("flutter-tools").setup {
+            lsp = {
+                -- this keeps the plugin in control of LSP setup
+                settings = {
+                    dart = {
+                        lineLength = 80, -- optional
+                    },
+                },
+            },
+            -- 👇 formatter settings
+            flutter_outline = {
+                enabled = true,
+            },
+            widget_guides = {
+                enabled = true,
+            },
+        }
+
+        -- 👇 Set tab size behavior for Dart specifically
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "dart",
+            callback = function()
+                vim.bo.shiftwidth = 2
+                vim.bo.tabstop = 2
+                vim.bo.softtabstop = 2
+                vim.bo.expandtab = true
+            end,
+        })
+    end,
 }
