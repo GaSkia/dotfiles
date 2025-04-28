5ae24d868d0869ff0c42880d719b6aeb09240f78
GaSkia <gabry.schiavo00@gmail.com>
2025-04-28 23:02:06 +0200

    added rust lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,24 +1,25 @@
 return {
     "neovim/nvim-lspconfig",
-    -- opts = {
-        -- inlay_hints = { enabled = true },
-        -- servers = {
-            -- pyright = {},
-            -- html = {},
-            -- ts_ls = {},
-            -- cssls = {},
-            -- clangd = {},
-            -- lua_ls = {},
-            -- omnisharp = {},
-        -- },
+    opts = {
+        inlay_hints = { enabled = true },
+        servers = {
+            pyright = {},
+            html = {},
+            ts_ls = {},
+            cssls = {},
+            clangd = {},
+            lua_ls = {
+                completion = true,
+            },
+            omnisharp = {},
+            rust_analyzer = {},
+        },
         -- setup = {},
-    -- },
-    config = function ()
+    },
+    config = function (_, opts)
         local lspconfig = require("lspconfig")
         lspconfig.pyright.setup{
---
         }
---
         lspconfig.html.setup({
             cmd = { "vscode-html-language-server", "--stdio" },
             filetypes = { "html", "htmldjango", "eelixir" },
@@ -50,6 +51,11 @@ return {
         }
         -- lspconfig.omnisharp.setup{}
         lspconfig.sqls.setup{}
+        lspconfig.rust_analyzer.setup{
+            on_attach = function(client, bufnr)
+                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr})
+            end,
+        }
     end,
 }
 -- "ts_ls",
