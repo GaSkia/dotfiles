bf6a52c86fe9f84040731e5369c7a42f363a533c
GaSkia <gabry.schiavo00@gmail.com>
2026-09-08 00:12:12 +0200

    fix: assign correct ln syntax

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ export XDG_CONFIG_HOME="$HOME/.config"
 
 sudo pacman -S --noconfirm neovim tmux quickshell hyprpaper hyprpicker
 
-ln -s "./alacritty" "$XDG_CONFIG_HOME/alacritty"
-ln -s "./hypr" "$XDG_CONFIG_HOME/hypr"
-ln -s "./nvim" "$XDG_CONFIG_HOME/nvim"
-ln -s "./tmux" "$XDG_CONFIG_HOME/tmux"
-ln -s "./waybar" "$XDG_CONFIG_HOME/waybar"
+ln -s "$HOME/dotfiles/alacritty" "$XDG_CONFIG_HOME/alacritty"
+ln -s "$HOME/dotfiles/hypr" "$XDG_CONFIG_HOME/hypr"
+ln -s "$HOME/dotfiles/nvim" "$XDG_CONFIG_HOME/nvim"
+ln -s "$HOME/dotfiles/tmux" "$XDG_CONFIG_HOME/tmux"
+ln -s "$HOME/dotfiles/waybar" "$XDG_CONFIG_HOME/waybar"
