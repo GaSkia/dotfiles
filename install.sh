5ca00f782bc7f29a211b337d59ae09fb56409003
gaskia <gabry.schiavo00@gmail.com>
2026-09-10 14:48:51 +0200

    improve install script

--- install.sh
+++ install.sh
@@ -2,10 +2,17 @@
 
 export XDG_CONFIG_HOME="$HOME/.config"
 
-sudo pacman -S --noconfirm neovim tmux quickshell hyprpaper hyprpicker
+sudo pacman -S --needed --noconfirm neovim tmux quickshell hyprpaper hyprpicker
 
+
+rm -rf "$XDG_CONFIG_HOME/alacritty"
+rm -rf "$XDG_CONFIG_HOME/hypr"
+rm -rf "$XDG_CONFIG_HOME/nvim"
+rm -rf "$XDG_CONFIG_HOME/tmux"
+rm -rf "$XDG_CONFIG_HOME/waybar"
 ln -s "$HOME/dotfiles/alacritty" "$XDG_CONFIG_HOME/alacritty"
 ln -s "$HOME/dotfiles/hypr" "$XDG_CONFIG_HOME/hypr"
 ln -s "$HOME/dotfiles/nvim" "$XDG_CONFIG_HOME/nvim"
 ln -s "$HOME/dotfiles/tmux" "$XDG_CONFIG_HOME/tmux"
 ln -s "$HOME/dotfiles/waybar" "$XDG_CONFIG_HOME/waybar"
+hyprctl reload
